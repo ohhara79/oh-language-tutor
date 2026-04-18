8c49773dcb3325929acf597f24b63a998086097a
Taeho Oh <ohhara@postech.edu>
2026-04-18 11:01:29 +0900

    Script minor update.

--- scripts/bladerunner.sh
+++ scripts/bladerunner.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-scummvm 2>&1 | uv run --frozen --no-dev main.py \
-            --source-language English \
-            --target-language Korean \
-            --level intermediate \
-            --extra-system-prompt extras/bladerunner.md \
-            --filter-regex '^\w+: "' \
-            --tui
+scummvm 2>&1 | \
+    uv run --frozen --no-dev main.py \
+        --source-language English \
+        --target-language Korean \
+        --level intermediate \
+        --extra-system-prompt extras/bladerunner.md \
+        --filter-regex '^\w+: "' \
+        --state-dir state/bladerunner \
+        --web \
+
