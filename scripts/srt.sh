63361b285a008b7947dd4bc2cb6e5c3f738116d4
Taeho Oh <ohhara@postech.edu>
2026-05-01 16:10:46 +0900

    Add srt.sh script to parse srt file.

---
+++ scripts/srt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat "$*" 2>&1 | \
+tr -d '\r' | \
+sed -e 's|</\?i>||g' | \
+awk 'BEGIN{RS=""; FS="\n"} {
+    text=$3
+    for (i=4; i<=NF; i++) text = text " " $i
+    print text
+}' | \
+    uv run --frozen --no-dev main.py \
+        --source-language English \
+        --target-language Korean \
+        --level intermediate \
+        --state-dir state/srt \
+        --web \
+
