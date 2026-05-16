5951a15991d086b3c4f400ce23cc65328b62e6ba
Taeho Oh <ohhara@postech.edu>
2026-05-16 20:32:25 +0900

    Remove source-language, target-language, level in cmd arg.

--- scripts/sword1.sh
+++ scripts/sword1.sh
@@ -2,9 +2,6 @@
 
 scummvm 2>&1 | \
     uv run --frozen --no-dev main.py \
-        --source-language English \
-        --target-language Korean \
-        --level intermediate \
         --extra-system-prompt extras/sword1.md \
         --filter-regex '^\w+: "' \
         --state-dir state/sword1 \
